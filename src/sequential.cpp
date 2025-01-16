c360d6227247dd80b66effb2392fffea474910ee
Emilio Garzia <emilio.garzia001@studenti.uniparthenope.it>
2025-01-16 17:13:50 +0100

    Refactoring

--- src/sequential.cpp
+++ src/sequential.cpp
@@ -1,148 +1,160 @@
 /*
- @brief Image cartoonization using K-means colors quantization: SEQUENTIAL CPU VERSION
- @file sequential.cpp
- @author Emilio Garzia, Luigi Marino
- @date 2025
- @details g++ sequential.cpp -o sequential -lopencv_core -lopencv_imgcodecs -lopencv_highgui -lopencv_imgproc
-*/
+ * Cartoonizer: Image quantization using K-Means algorithms
+ * @autors Emilio Garzia, Luigi Marino 
+ */
 
 #include <opencv2/opencv.hpp>
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <limits>
 #include <stdlib.h>
-#include <math.h>
 #include <time.h>
+#include <unistd.h>
 
-typedef struct {
+// Default parameters
+#define DEFAULT_CLUSTERS 7
+#define DEFAULT_ITERATIONS 50
+#define DEFAULT_SEED 200
+#define DEFAULT_THREADS 256
+
+struct Color {
     float r, g, b;
-} Color;
+};
 
-// Assign pixels to centroids routine
-void assign_pixels_to_centroids(Color* pixels, Color* centroids, int* assignments, int num_pixels, int num_centroids) {
-    for (int i = 0; i < num_pixels; i++) {
-        float min_dist = FLT_MAX;
-        int closest_centroid = 0;
-        Color pixel = pixels[i];
-
-        for (int j = 0; j < num_centroids; j++) {
-            float dist = (pixel.r - centroids[j].r) * (pixel.r - centroids[j].r) +
-                         (pixel.g - centroids[j].g) * (pixel.g - centroids[j].g) +
-                         (pixel.b - centroids[j].b) * (pixel.b - centroids[j].b);
-            if (dist < min_dist) {
-                min_dist = dist;
-                closest_centroid = j;
-            }
-        }
-        assignments[i] = closest_centroid;
-    }
+// Compute the Euclidean distance between two colors
+float euclidean_distance(const Color& a, const Color& b) {
+    return std::sqrt((a.r - b.r) * (a.r - b.r) + (a.g - b.g) * (a.g - b.g) + (a.b - b.b) * (a.b - b.b));
 }
 
-// Update centroids routine
-void update_centroids(Color* pixels, int* assignments, Color* centroids, int* cluster_sizes, int num_pixels, int num_centroids) {
-    // Reset centroids and dimensions of the clusters
-    for (int i = 0; i < num_centroids; i++) {
-        centroids[i].r = 0.0f;
-        centroids[i].g = 0.0f;
-        centroids[i].b = 0.0f;
-        cluster_sizes[i] = 0;
-    }
+// Update centroids based on pixel assignments
+void update_centroids(const std::vector<Color>& pixels, const std::vector<int>& assignments, std::vector<Color>& centroids, int k) {
+    std::vector<int> cluster_sizes(k, 0);
+    std::vector<Color> new_centroids(k, {0.0f, 0.0f, 0.0f});
 
-    // Update centroids
-    for (int i = 0; i < num_pixels; i++) {
+    for (size_t i = 0; i < pixels.size(); i++) {
         int cluster_idx = assignments[i];
-        centroids[cluster_idx].r += pixels[i].r;
-        centroids[cluster_idx].g += pixels[i].g;
-        centroids[cluster_idx].b += pixels[i].b;
+        new_centroids[cluster_idx].r += pixels[i].r;
+        new_centroids[cluster_idx].g += pixels[i].g;
+        new_centroids[cluster_idx].b += pixels[i].b;
         cluster_sizes[cluster_idx]++;
     }
 
-    // Compute the new average value
-    for (int i = 0; i < num_centroids; i++) {
+    for (int i = 0; i < k; i++) {
         if (cluster_sizes[i] > 0) {
-            centroids[i].r /= cluster_sizes[i];
-            centroids[i].g /= cluster_sizes[i];
-            centroids[i].b /= cluster_sizes[i];
+            new_centroids[i].r /= cluster_sizes[i];
+            new_centroids[i].g /= cluster_sizes[i];
+            new_centroids[i].b /= cluster_sizes[i];
         }
     }
+
+    centroids = new_centroids;
 }
 
-// Routine that make the output image
-void create_cartoon_image(Color* pixels, int* assignments, Color* centroids, int num_pixels) {
-    for (int i = 0; i < num_pixels; i++) {
-        Color pixel = pixels[i];
-        int closest_centroid = assignments[i];
-
-        pixel.r = centroids[closest_centroid].r;
-        pixel.g = centroids[closest_centroid].g;
-        pixel.b = centroids[closest_centroid].b;
-
-        pixels[i] = pixel;
-    }
-}
-
-// driver code
-int main() {
-    const int num_clusters = 20; 
-    const int max_iterations = 50;
-
-    cv::Mat image = cv::imread("images/image.jpg");
-
-    if (image.empty()) {
-        printf("ERROR: An error were occurred during the image loading\n");
-        return -1;
-    }
-
+// K-means algorithm for color quantization
+void kmeans_cpu(const cv::Mat& image, int k, int max_iter, cv::Mat& output_image,int seed) {
     int width = image.cols;
     int height = image.rows;
     int num_pixels = width * height;
 
-    double start = (double)clock()/CLOCKS_PER_SEC;
-
-    Color* pixels = (Color*)malloc(num_pixels * sizeof(Color));
-    for (int i = 0; i < num_pixels; i++) {
-        cv::Vec3b color = image.at<cv::Vec3b>(i / width, i % width);
-        pixels[i].b = (float)color[0] / 255.0f;
-        pixels[i].g = (float)color[1] / 255.0f;
-        pixels[i].r = (float)color[2] / 255.0f;
+    // Convert image to a vector of Color structs
+    std::vector<Color> pixels(num_pixels);
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            cv::Vec3b color = image.at<cv::Vec3b>(i, j);
+            pixels[i * width + j] = { color[2] / 255.0f, color[1] / 255.0f, color[0] / 255.0f };
+        }
     }
 
-    srand(42);
+    srand(seed);
 
-    Color* centroids = (Color*)malloc(num_clusters * sizeof(Color));
-    for (int i = 0; i < num_clusters; i++) {
-        centroids[i].r = (float)rand() / RAND_MAX;
-        centroids[i].g = (float)rand() / RAND_MAX;
-        centroids[i].b = (float)rand() / RAND_MAX;
+    // Randomly initialize centroids
+    std::vector<Color> centroids(k);
+    for (int i = 0; i < k; i++) {
+        centroids[i] = { float(rand()) / RAND_MAX, float(rand()) / RAND_MAX, float(rand()) / RAND_MAX };
     }
 
-    int* assignments = (int*)malloc(num_pixels * sizeof(int));
-    int* cluster_sizes = (int*)malloc(num_clusters * sizeof(int));
+    std::vector<int> assignments(num_pixels, 0);
+    for (int iter = 0; iter < max_iter; iter++) {
+        // Step 1: Assign each pixel to the closest centroid
+        for (int i = 0; i < num_pixels; i++) {
+            float min_dist = std::numeric_limits<float>::max();
+            for (int j = 0; j < k; j++) {
+                float dist = euclidean_distance(pixels[i], centroids[j]);
+                if (dist < min_dist) {
+                    min_dist = dist;
+                    assignments[i] = j;
+                }
+            }
+        }
 
-    // K-means execution
-    for (int iter = 0; iter < max_iterations; iter++) {
-        assign_pixels_to_centroids(pixels, centroids, assignments, num_pixels, num_clusters);
-        update_centroids(pixels, assignments, centroids, cluster_sizes, num_pixels, num_clusters);
+        // Step 2: Update centroids based on the pixel assignments
+        update_centroids(pixels, assignments, centroids, k);
     }
 
-    create_cartoon_image(pixels, assignments, centroids, num_pixels);
-    for (int i = 0; i < num_pixels; i++) {
-        cv::Vec3b new_color(
-            (unsigned char)(pixels[i].b * 255),
-            (unsigned char)(pixels[i].g * 255),
-            (unsigned char)(pixels[i].r * 255)
-        );
-        image.at<cv::Vec3b>(i / width, i % width) = new_color;
+    // Step 3: Create the output image
+    output_image = image.clone();
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            int cluster_idx = assignments[i * width + j];
+            cv::Vec3b new_color(
+                static_cast<unsigned char>(centroids[cluster_idx].b * 255),
+                static_cast<unsigned char>(centroids[cluster_idx].g * 255),
+                static_cast<unsigned char>(centroids[cluster_idx].r * 255)
+            );
+            output_image.at<cv::Vec3b>(i, j) = new_color;
+        }
+    }
+}
+
+// Argument parser
+void arg_parser(int argc,char* argv[], int& clusters, int& iterations, int& seed, int& threads_per_block,std::string& input_image_path, std::string& output_image_path){
+    int opt;
+    while ((opt = getopt(argc, argv, "c:i:s:t:")) != -1) {
+        switch (opt) {
+            case 'c':
+                clusters = std::stoi(optarg);
+                break;
+            case 'i':
+                iterations = std::stoi(optarg);
+                break;
+            case 's':
+                seed = std::stoi(optarg);
+                break;
+            case 't':
+                threads_per_block = std::stoi(optarg);
+                break;
+            default:
+                std::cerr << "Usage: " << argv[0] << " [-c clusters] [-i iterations] [-s seed] [-t threads] <input_image> <output_image>" << std::endl;
+        }
+    }
+    if (optind < argc) input_image_path = argv[optind++];
+    if (optind < argc) output_image_path = argv[optind++];
+}
+
+//driver code
+int main(int argc, char* argv[]) {
+    int clusters = DEFAULT_CLUSTERS;
+    int iterations = DEFAULT_ITERATIONS;
+    int seed = DEFAULT_SEED;
+    int threads_per_block = DEFAULT_THREADS;
+    std::string input_image_path = "images/image.jpg";
+    std::string output_image_path = "images/cartoon_cpu.jpg";
+
+    arg_parser(argc, argv, clusters, iterations, seed, threads_per_block, input_image_path, output_image_path);
+
+    // Load the image
+    cv::Mat image = cv::imread(input_image_path);
+    if (image.empty()) {
+        std::cerr << "Error loading image!" << std::endl;
+        return -1;
     }
 
-    cv::imwrite("images/cartoon_image.jpg", image);
+    cv::Mat output_image;
+    kmeans_cpu(image, clusters, iterations, output_image, seed);
 
-    double end = (double)clock()/CLOCKS_PER_SEC;
-    double delta = end-start;
-    std::cout << "Execution time: " << delta << "s" << std::endl;
-  
-    free(pixels);
-    free(centroids);
-    free(assignments);
-    free(cluster_sizes);
-
+    // Save the output
+    cv::imwrite(output_image_path, output_image);
+    std::cout << "CPU output saved!" << std::endl;
     return 0;
 }
